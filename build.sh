32ee90b4f3543d83c1e7f3d1a4f4124c9647da79
sisit135 <sisit-ton@outlook.com>
2022-10-14 21:01:10 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,12 +3,20 @@ printf 'Installing dependencies...\n'
 
 curl -L -o ffmpeg_tmp.tar.xz https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-linux64-lgpl-shared.tar.xz && tar -xf ffmpeg_tmp.tar.xz
 chmod +x ./ffmpeg-master-latest-linux64-lgpl-shared/bin/ffmpeg
-export PATH=$PATH:./ffmpeg-master-latest-linux64-lgpl-shared/bin/
+export PATH="$PATH:./ffmpeg-master-latest-linux64-lgpl-shared/bin/"
+echo $PATH
 
 yarn
 cd gulp
 yarn
 printf 'Building...\n'
-yarn gulp build.web-shapezio
 
-printf 'Done.\n'
+yarn gulp build.web-shapezio
+if [ $? -eq 0 ]
+then
+  printf 'Done.\n'
+  exit 0
+else
+  echo "Build Script failed" >&2
+  exit 1
+fi
