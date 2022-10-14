73e54d83048054962b5cf701e817015e748ed798
sisit135 <sisit-ton@outlook.com>
2022-10-14 20:43:27 +0700

    oops forgot -L

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 printf 'Installing dependencies...\n'
 
-curl -o ffmpeg_tmp.tar.xz https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-linux64-lgpl-shared.tar.xz && tar -xf ffmpeg_tmp.tar.xz
+curl -L -o ffmpeg_tmp.tar.xz https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-linux64-lgpl-shared.tar.xz && tar -xf ffmpeg_tmp.tar.xz
 chmod +x ./ffmpeg-master-latest-linux64-lgpl-shared/bin/ffmpeg
 export PATH=$PATH:./ffmpeg-master-latest-linux64-lgpl-shared/bin/
 
